58f0ab2e85746d942a073c115be097c36ffc74f1
Rakhi <rakhichd@berkeley.edu>
2024-10-18 23:11:47 -0700

    start proj

---
+++ Spot the Op/Spot the Op/Spot_the_OpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spot_the_OpApp.swift
+//  Spot the Op
+//
+//  Created by rakhi c on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Spot_the_OpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Spot the Op/Spot the OpTests/Spot_the_OpTests.swift
@@ -0,0 +1,17 @@
+//
+//  Spot_the_OpTests.swift
+//  Spot the OpTests
+//
+//  Created by rakhi c on 10/18/24.
+//
+
+import Testing
+@testable import Spot_the_Op
+
+struct Spot_the_OpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
